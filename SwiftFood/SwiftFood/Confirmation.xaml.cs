181f1454ff0cd07c688df7409901615c88e74f50
MihirGit55 <mihirraniga@hotmail.co.uk>
2021-11-22 20:38:48 +0000

    Changes 22/11/21
    Mostly adding to the checkout page. Also creating a confirmation page to be displayed after checkout

---
+++ SwiftFood/SwiftFood/Confirmation.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SwiftFood
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Confirmation : ContentPage
+    {
+        public Confirmation()
+        {
+            InitializeComponent();
+        }
+
+        private async void btn_Return2Browse_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new Browse()));
+        }
+    }
+}
